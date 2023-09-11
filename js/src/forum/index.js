a830750c54bf5629efa430ed8105a1183ea8fe8d
emin.lin <emin.lin@live.com>
2023-09-11 10:13:12 +0800

    修复表情失效

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -21,13 +21,13 @@ function loadEmojioneArea() {
         style.rel = 'stylesheet';
         style.crossOrigin = 'anonymous';
         // style.integrity = 'sha256-LKawN9UgfpZuYSE2HiCxxDxDgLOVDx2R4ogilBI52oc=';
-        style.href = 'https://oss.hamzone.cn/static/emojionearea%403.4.2/emojionearea.min.css';
+        style.href = 'https://oss.hamcq.cn/static/emojionearea%403.4.2/emojionearea.min.css';
         document.head.appendChild(style);
 
         const script = document.createElement('script');
         script.crossOrigin = 'anonymous';
         // script.integrity = 'sha256-ImIFrmJd7ymGlVw2MbtI96BNPW4NfcKqM3d1Go665Ig=';
-        script.src = 'https://oss.hamzone.cn/static/emojionearea%403.4.2/emojionearea.min.js';
+        script.src = 'https://oss.hamcq.cn/static/emojionearea%403.4.2/emojionearea.min.js';
         script.onload = resolve;
         document.body.appendChild(script);
     });
